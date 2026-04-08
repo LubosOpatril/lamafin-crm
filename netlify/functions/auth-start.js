da26ba4619c849c3e97f0310ee15b342a5d8fc28
LubosOpatril <lubos.opatril@lamafin.cz>
2026-04-08 16:26:22 +0200

    Update auth-start.js

--- netlify/functions/auth-start.js
+++ netlify/functions/auth-start.js
@@ -2,14 +2,31 @@ exports.handler = async () => {
   const client_id = process.env.GOOGLE_CLIENT_ID;
   const redirect_uri = process.env.REDIRECT_URI;
 
-  const scope = encodeURIComponent("https://www.googleapis.com/auth/gmail.readonly");
+  if (!client_id || !redirect_uri) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({
+        error: "Chybí GOOGLE_CLIENT_ID nebo REDIRECT_URI",
+        hasClientId: !!client_id,
+        redirectUri: redirect_uri || null
+      }),
+    };
+  }
 
-  const url = `https://accounts.google.com/o/oauth2/v2/auth?client_id=${client_id}&redirect_uri=${redirect_uri}&response_type=code&scope=${scope}&access_type=offline&prompt=consent`;
+  const params = new URLSearchParams({
+    client_id,
+    redirect_uri,
+    response_type: "code",
+    scope: "https://www.googleapis.com/auth/gmail.readonly",
+    access_type: "offline",
+    prompt: "consent",
+  });
 
   return {
     statusCode: 302,
     headers: {
-      Location: url,
+      Location: `https://accounts.google.com/o/oauth2/v2/auth?${params.toString()}`,
     },
   };
+};
 };
